730525757eaea680f44eca6149596333c424de0d
LAILA CALDERON <laila.calderon@udgvirtual.udg.mx>
2023-09-30 13:51:40 -0600

    Primer commit

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { IContact } from 'src/app/models/IContact';
+import { ContactService } from 'src/app/services/contact.service';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  public loading: boolean = false;
+  public contacts: IContact[] = [];
+  public errorMessage: string | null = null;
+
+  constructor(private contactService: ContactService){
+
+  }
+
+  ngOnInit(): void{
+    this.getAllContactsFromServer();
+  }
+
+  public getAllContactsFromServer(){
+    this.loading=true;
+    this.contactService.getAllContacts().subscribe((data : IContact[]) => {
+      this.contacts = data;
+      this.loading = false;
+    }, (error) => {
+      this.errorMessage = error;
+      this.loading = false;
+    });
+  }
+
+  public clickDeleteContact(contactId : string | undefined){
+    if(contactId){
+      this.contactService.deleteContact(contactId).subscribe((data:{}) =>{
+        this.getAllContactsFromServer();
+      }, (error) =>{
+        this.errorMessage = error;
+      });
+      
+    }
+  }
+
+}
---
+++ src/app/models/IGroup.ts
@@ -0,0 +1,9 @@
+export interface IGroup {
+    id: string;
+    name: string;
+    email: string;
+    photo: string;
+    company: string;
+    title: string;
+    groupId: string;
+}
